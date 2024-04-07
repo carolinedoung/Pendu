ca3dfe5a94d2603f166b34ef448a2ca16b319236
carolinedoung <caroline.doung@edu.univ-eiffel.fr>
2024-04-07 16:04:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 
-
 function App() {
   const [mot, setMot] = useState("");
   const [lettresDevinees, setLettresDevinees] = useState([]);
   const [essais, setEssais] = useState(6);
 
   useEffect(() => {
+    demarrerNouveauJeu();
+  }, []);
+
+  function demarrerNouveauJeu() {
     fetch('https://node-hangman-api-production.up.railway.app/', {
       method: 'POST',
       headers: {
@@ -19,12 +22,12 @@ function App() {
     })
       .then(response => response.json())
       .then(data => {
-        console.log(data);
-      const word = data.word.toUpperCase(); // Convertir le mot en majuscule
-      setMot(word);
-      setLettresDevinees([word[0]]);
+        const word = data.word.toUpperCase();
+        setMot(word);
+        setLettresDevinees([word[0]]);
+        setEssais(6);
       });
-  }, []);
+  }
 
   function handleClicLettre(lettre) {
     if (!lettresDevinees.includes(lettre)) {
@@ -37,25 +40,37 @@ function App() {
   } 
 
   if (essais === 0) {
-    return <p>Vous avez perdu ! Le mot était {mot}.</p>;
+    return (
+      <div>
+        <p>Vous avez perdu ! Le mot était {mot}.</p>
+        <button onClick={demarrerNouveauJeu}>Rejouer</button>
+      </div>
+    );
   }
 
   if (mot.split('').filter(lettre => lettre !== '-').every(lettre => lettresDevinees.includes(lettre))) {
-    return <p>Bravo ! Vous avez deviné le mot {mot}.</p>;
+    return (
+      <div>
+        <p>Bravo ! Vous avez deviné le mot {mot}.</p>
+        <button onClick={demarrerNouveauJeu}>Rejouer</button>
+      </div>
+    );
   }
+
   return (
     <div>
       <h1>Jeu du pendu</h1>
-        <p>Mot à deviner: {mot.split('').map(lettre => lettresDevinees.includes(lettre) || lettre === '-' ? lettre : '_').join(' ')}</p>        
-        <p>Lettres déjà devinées: {lettresDevinees.join(', ')}</p>
-        <p>Essais restants: {essais}</p>
-        <p>Cliquez sur une lettre pour la deviner:</p>
-        {'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(lettre =>
-          <button key={lettre} onClick={() => handleClicLettre(lettre)}>
-            {lettre}
-          </button>
-        )}
+      <p>Mot à deviner: {mot.split('').map(lettre => lettresDevinees.includes(lettre) || lettre === '-' ? lettre : '_').join(' ')}</p>        
+      <p>Lettres déjà devinées: {lettresDevinees.join(', ')}</p>
+      <p>Essais restants: {essais}</p>
+      <p>Cliquez sur une lettre pour la deviner:</p>
+      {'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(lettre =>
+        <button key={lettre} onClick={() => handleClicLettre(lettre)}>
+          {lettre}
+        </button>
+      )}
     </div>
   );
 }
+
 export default App;
